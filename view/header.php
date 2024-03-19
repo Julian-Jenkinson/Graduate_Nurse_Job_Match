ee8f5a74e8ed52413c88da258bef613e892adcbf
Dionnybel Sarate <u1145361@umail.usq.edu.au>
2024-03-19 20:47:10 +1000

    Update 'view/header.php'

--- view/header.php
+++ view/header.php
@@ -1,4 +1,37 @@
 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Graduate Nurse/Midwife Job Matching</title>
+    <link rel="stylesheet" href="homepage.css" type="text/css"/>
+    <script type="text/javascript" src="homepage.js"></script>     
+
+</head>
+<body>
+    <header>
+        <div class="header-logo">
+            <img src="./Images/LOGO.png" alt="Graduate Nursing/Midwife Job Match Logo" class="logo"/>
+            <div class="logo-text">
+                <span class="graduate">GR<span class="highlight-logo">A</span>DU<span class="highlight-logo">A</span>TE</span>
+                <span class="job-match">Nurse/Midwife Job Match</span>   
+            </div>         
+        </div>
+        <nav class="header-nav">
+            <ul>
+                <li><a href="home">Home</a></li>
+                <li><a href="jobs">Jobs</a></li>
+                <li><a href="#profile">Profile</a></li>
+            </ul>
+        </nav>
+        <div class="header-access">
+            <a href="user" class="signin-btn signin-btn::after">Sign In</a>
+            <a href="employer" class="employer-site-btn">Employer Site</a>
+        </div>
+    </header>
+<body>
+
+<!--
+<!DOCTYPE html>
 <html>
 
 <head>
@@ -9,9 +42,10 @@
     <meta charset="utf-8">
 
     <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+    -->
 
     <!-- this could be moved to the header-->
-    <script>(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+<!--<script>(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
         ({key: "AIzaSyCCggxqHlE1-kOJlato8lAsQeLcfG9eRj0", v: "weekly"});</script>
 </head>
 
@@ -27,6 +61,6 @@
     
     <hr>
 </header>
-
+-->
 
 
