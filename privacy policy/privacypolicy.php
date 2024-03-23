ce683f98e030c717bcb96e16323fefcac9bfd797
Dionnybel Sarate <u1145361@umail.usq.edu.au>
2024-03-23 22:31:10 +1000

    Updated

---
+++ privacy policy/privacypolicy.php
@@ -0,0 +1,45 @@
+<?php include '../view/header.php'; ?>
+
+<link rel="stylesheet" href="privacypolicy.css" type="text/css"/>
+
+  <div class="wave-background">
+    <section id="privacy-policy">
+      <h1>Privacy Policy</h1>
+      <p>Welcome to the Graduate Nurse/Midwife Job Match Privacy Policy. We are committed to protecting your personal information and respecting your privacy.</p>
+        
+      <h2>Information Collection</h2>
+      <p>We collect information to provide better services to all users of the Graduate Nurse/Midwife Job Match platform. The types of information we collect include:</p>
+      <ul>
+        <li>Personal information, such as names, email addresses, and professional details, when you register for our service.</li>
+        <li>Usage data and preferences to improve our service offerings.</li>
+        <li>Feedback and interactions with our platform to address your inquiries and improve user experience.</li>
+      </ul>
+
+      <h2>Use of Information</h2>
+      <p>The information we collect is used to:</p>
+      <ul>
+        <li>Operate, maintain, and provide the features and functionality of the Graduate Nurse/Midwife Job Match platform.</li>
+        <li>Understand and analyze how you use our platform to improve our services.</li>
+        <li>Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the platform, and for marketing and promotional purposes.</li>
+      </ul>
+
+      <h2>Data Sharing and Disclosure</h2>
+      <p>Your personal information is not shared outside the Graduate Nurse/Midwife Job Match project without your permission, except under the following circumstances:</p>
+      <ul>
+        <li>With your consent or at your direction.</li>
+        <li>As required by law, such as to comply with a subpoena or similar legal process.</li>
+        <li>To enforce our terms and conditions or protect our operations or users.</li>
+      </ul>
+
+      <h2>Your Rights</h2>
+      <p>You have the right to access, update, or delete the information we have on you. You can do this by logging into your account or contacting us directly.</p>
+
+      <h2>Changes to This Policy</h2>
+      <p>We may update this privacy policy to reflect changes to our information practices. If we make any material changes, we will notify you by email (sent to the e-mail address specified in your account) or by means of a notice on this platform prior to the change becoming effective.</p>
+
+      <h2>Contact Us</h2>
+      <p>If you have any questions about this Privacy Policy, please contact us.</p>
+    </section>
+  </div>
+  
+<?php include '../view/footer.php'; ?>
