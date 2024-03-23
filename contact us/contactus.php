420a96bb20641576ce708f970515314d93638be8
Dionnybel Sarate <u1145361@umail.usq.edu.au>
2024-03-23 23:02:35 +1000

    Updated

---
+++ contact us/contactus.php
@@ -0,0 +1,25 @@
+<?php include '../view/header.php'; ?>
+
+<link rel="stylesheet" href="contactus.css" type="text/css"/>
+
+    <div class="wave-background">
+      <section id="contact-info">
+            <h1>Contact Us</h1>
+            <p>If you have any questions, comments, or would like to get in touch with the Graduate Nurse/Midwife Job Match team, please don't hesitate to reach out to us.</p>
+            
+            <div class="contact-method">
+                <h2>Project Supervisors</h2>
+                <p><strong>Dr. Lisa Beccaria</strong><br>
+                Associate Professor (Nursing)<br>
+                Email: <a href="mailto:Lisa.Beccaria@unisq.edu.au">Lisa.Beccaria@unisq.edu.au</a><br>
+                Phone: +61 7 4631 2753</p>
+
+                <p><strong>Dr. Helen Donovan</strong><br>
+                  Senior Lecturer (Nursing)<br>
+                  Email: <a href="mailto:h.donovan@qut.edu.au">h.donovan@qut.edu.au</a><br>
+                  Phone: +61 7 3138 3719</p>
+            </div>
+      </section>
+  </div>
+
+<?php include '../view/footer.php'; ?>
