5e6772b3492245acb143bc6f9df31666d8fe2e05
Dionnybel Sarate <u1145361@umail.usq.edu.au>
2024-03-23 22:32:11 +1000

    Updated

---
+++ t and c/tandc.php
@@ -0,0 +1,34 @@
+<?php include '../view/header.php'; ?>
+
+<link rel="stylesheet" href="tandc.css" type="text/css"/>
+
+  <div class="wave-background">
+    <section id="terms-and-conditions">
+      <h1>Terms and Conditions</h1>
+      <p>Welcome to the Graduate Nurse/Midwife Job Match platform. These terms and conditions outline the rules and regulations for the use of the University of Southern Queensland's Job Match platform, found at:</p>
+            
+      <p><a href="https://www.unisq.edu.au/">https://www.unisq.edu.au/</a></p>
+
+      <p>By accessing this platform, we assume you accept these terms and conditions. Do not continue to use the Graduate Nurse/Midwife Job Match platform if you do not agree to all of the terms and conditions stated on this page.</p>
+
+      <h2>Intellectual Property</h2>
+      <p>The content, layout, design, data, databases, and graphics on this platform are protected by intellectual property laws. Unless explicitly stated, you do not have rights to any of these materials. You may not copy, broadcast, transmit, adapt, or change the content or graphics in any way, for any purpose, without prior written consent from the University of Southern Queensland.</p>
+
+      <h2>User Responsibilities</h2>
+      <p>As a user of this platform, you agree to provide truthful and accurate information. You agree to use the website only for lawful purposes and in a way that does not infringe upon the rights of or restrict or inhibit the use and enjoyment of this site by any third party.</p>
+
+      <h2>Disclaimer</h2>
+      <p>The University of Southern Queensland and its employees do not guarantee the completeness, reliability, or accuracy of the information presented. Any action you take upon the information found on this platform is strictly at your own risk, and we will not be liable for any losses or damages in connection with the use of our platform.</p>
+
+      <h2>Governing Law</h2>
+      <p>These terms and conditions are governed by and construed in accordance with the laws of Queensland, Australia, and you irrevocably submit to the exclusive jurisdiction of the courts in that State or location.</p>
+
+      <h2>Changes to Terms</h2>
+      <p>The University of Southern Queensland reserves the right to revise these terms and conditions at any time. By using this platform, you are expected to review these terms on a regular basis.</p>
+
+      <h2>Contact Information</h2>
+      <p>If you have any queries regarding any of our terms, please contact us.</p>
+    </section>
+  </div>
+
+<?php include '../view/footer.php'; ?>
