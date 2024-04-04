07c4190db6140e1ee2dc0fdf4f193e7e94b4b97b
Dionnybel Sarate <u1145361@umail.usq.edu.au>
2024-04-04 16:52:38 +1000

    Updated for CSS

--- user/user_signup.php
+++ user/user_signup.php
@@ -3,27 +3,30 @@
 
 
 <?php include '../view/header.php'; ?>
+<link rel="stylesheet" href="user_signup.css" type="text/css"/>
 
-<h3>User sign up</h3>
+<div class="form-container">
+    <h3>User sign up</h3>
 
-<br>
-<form action="." method="post">
-    <label>Email:</label><br>
-    <input type="text" name="email">
-    <br><br>
-    <label>Password:</label><br>
-    <input type="password" name="password1">
-    <br><br>
-    <label>Confirm Password:</label><br>
-    <input type="password" name="password2">
-    <br><br>
+    <br>
+    <form action="." method="post">
+        <label>Email:</label><br>
+        <input type="text" name="email">
+        <br><br>
+        <label>Password:</label><br>
+        <input type="password" name="password1">
+        <br><br>
+        <label>Confirm Password:</label><br>
+        <input type="password" name="password2">
+        <br><br>
 
-    <input type="submit" value="Sign up">
-    <input type="hidden" name="action" value="add_user">
-    <br><br>
-</form>
+        <input type="submit" value="Sign up">
+        <input type="hidden" name="action" value="add_user">
+        <br><br>
+    </form>
 
-<p>Allready have an account? <a href="user_login.php">login</a></p>
-<br><br>
+    <p>Allready have an account? <a href="user_login.php">login</a></p>
+    <br><br>
+</div>
 
 <?php include '../view/footer.php'; ?>
