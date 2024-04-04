b9b2b77ecc617ac9f415ef1bcb1b1dbab3b34a1e
Dionnybel Sarate <u1145361@umail.usq.edu.au>
2024-04-04 16:54:09 +1000

    Updated for CSS

--- user/user_profile.php
+++ user/user_profile.php
@@ -1,41 +1,46 @@
 <?php include '../view/header.php'; ?>
+<link rel="stylesheet" href="user_profile.css" type="text/css"/>
 
-<br>
-<div>You are logged in as <?php echo $_SESSION['user']['userEmail']; ?></div> 
 
-<h3>User Profile</h3>
+<div class="main-container">
+    <div class="logged-in-notice">
+        You are logged in as <?php echo $_SESSION['user']['userEmail']; ?>
+    </div> 
 
-<!-- create profile button -->
-<?php if(!isset($user['userFName'])): ?>
-    <p>Get started by creating your profile</p>
+    <h3>User Profile</h3>
 
-    <form action="." method="post">
-        <input type="submit" value="Create Profile"/>
-        <input type="hidden" name="action" value="create_profile_button">
-    </form><br><br>
-<?php endif; ?>
+    <!-- create profile button -->
+    <?php if(!isset($user['userFName'])): ?>
+        <p>Get started by creating your profile</p>
 
-<!-- display user profile -->
+        <form action="." method="post">
+            <input type="submit" value="Create Profile"/>
+            <input type="hidden" name="action" value="create_profile_button">
+        </form><br><br>
+    <?php endif; ?>
 
-<?php if(isset($user['userFName'])): ?>
-    <h4>Your Details</h4>
+    <!-- display user profile -->
 
-    <div>Name:</div><div><?php echo $user['userFName'] . ' ' . $user['userLName']; ?></div><br>
-    <div>Address:</div><div><?php echo $user['userAddress']; ?></div><br>
-    <div>Qualifications:</div><div><?php echo $user['userQualifications']; ?></div><br>
-    <div>Position interested in:</div><div><?php echo $user['jobTitle']; ?></div><br>
-    
-    <h4>Professional Interests:</h4>
-    <p>in progress</p>
+    <?php if(isset($user['userFName'])): ?>
+        <h4>Your Details</h4>
 
-    <h4>Locality Preferences:</h4>
-    <p>in progress</p>
+        <div>Name:</div><div><?php echo $user['userFName'] . ' ' . $user['userLName']; ?></div><br>
+        <div>Address:</div><div><?php echo $user['userAddress']; ?></div><br>
+        <div>Qualifications:</div><div><?php echo $user['userQualifications']; ?></div><br>
+        <div>Position interested in:</div><div><?php echo $user['jobTitle']; ?></div><br>
+        
+        <h4>Professional Interests:</h4>
+        <p>in progress</p>
 
-    <form action="." method="post">
-        <input type="submit" value="Edit Profile"/>
-        <input type="hidden" name="action" value="edit_profile">
-    </form><br><br>
+        <h4>Locality Preferences:</h4>
+        <p>in progress</p>
 
-<?php endif; ?>
+        <form action="." method="post">
+            <input type="submit" value="Edit Profile"/>
+            <input type="hidden" name="action" value="edit_profile">
+        </form><br><br>
+
+    <?php endif; ?>
+</div>
 
 <?php include '../view/footer.php'; ?>
