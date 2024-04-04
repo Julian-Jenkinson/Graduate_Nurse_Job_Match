0a7f6e387668e97dcae1c8612d1b268af9d2d50a
Dionnybel Sarate <u1145361@umail.usq.edu.au>
2024-04-04 22:25:17 +1000

    Updated for CSS

--- employer/employer_login.php
+++ employer/employer_login.php
@@ -1,29 +1,29 @@
 <?php include '../view/employer_header.php'; ?>
+<link rel="stylesheet" href="employer_login.css" type="text/css"/>
 
-<h3>Employer log in</h3>
-<br>
+<div class="form-container">
+    <h3>Employer log in</h3>
+    <br>
 
-<form action="." method="post">
-    <label>Email:</label><br>
-    <input type="text", name="email">
+    <form action="." method="post">
+        <label>Email:</label><br>
+        <input type="text", name="email">
+        <br><br>
+        <label>Password:</label><br>
+        <input type="password", name="password" >
+        <!-- forgot password button? -->
+        <br><br>
+        <input type="submit" value="Log in as Employer">
+        <input type="hidden" name="action" value="get_employer">
+        <br><br>
+    </form>
+
+    <p>Dont have an employer account yet? <a href="employer_signup.php">Sign up</a></p>
     <br><br>
-    <label>Password:</label><br>
-    <input type="password", name="password" >
-    <!-- forgot password button? -->
+    <p>Are you looking for jobs?</p>
+    <p><a href="../user/">Log in</a> or <a href="../user/user_signup.php">sign up</a> as a job seeker</p>
     <br><br>
-    <input type="submit" value="Log in as Employer">
-    <input type="hidden" name="action" value="get_employer">
-    <br><br>
-</form>
-
-<p>Dont have an employer account yet? <a href="employer_signup.php">Sign up</a></p>
-<br><br>
-
-<p>Are you looking for jobs?</p>
-<p><a href="../user/">Log in</a> or <a href="../user/user_signup.php">sign up</a> as a job seeker</p>
-<br><br>
-
-
+</div>
 
 <?php include '../view/employer_footer.php'; ?>
 
