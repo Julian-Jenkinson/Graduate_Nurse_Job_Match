1573aa85ecbaf4522c442d95e49d324f335942a2
Dionnybel Sarate <u1145361@umail.usq.edu.au>
2024-04-04 16:55:23 +1000

    Updated for CSS

--- user/user_login.php
+++ user/user_login.php
@@ -1,25 +1,27 @@
 <?php include '../view/header.php'; ?>
+<link rel="stylesheet" href="user_login.css" type="text/css"/>
 
-<h3>User log in</h3>
-<br>
+<div class="form-container">
+    <h3>User log in</h3>
+    <br>
 
-<form action="." method="post">
-    <label>Email:</label><br>
-    <input type="text", name="email" >
+    <form action="." method="post">
+        <label>Email:</label><br>
+        <input type="text", name="email" >
+        <br><br>
+        
+        <label>Password:</label><br>
+        <input type="password" name="password">
+        <br><br>
+
+        <input type="submit" value="Log in">
+        <input type="hidden" name="action" value="get_user">
+        <br><br>
+    </form>
+
+    <p>Dont have an account yet? <a href="user_signup.php">Sign up</a></p>
     <br><br>
-    
-    <label>Password:</label><br>
-    <input type="password" name="password">
-    <!-- forgot password button? -->
-    <br><br>
+</div>
 
-    <input type="submit" value="Log in">
-    <input type="hidden" name="action" value="get_user">
-    <br><br>
-</form>
-
-
-<p>Dont have an account yet? <a href="user_signup.php">Sign up</a></p>
-<br><br>
 
 <?php include '../view/footer.php'; ?>
